d318cb6bda8d21e0bb2f33e11d30dbb2afc60e63
miladpeagh <miladpeagh1999@gmail.com>
2021-10-02 20:31:55 +0330

    store requireVal method

---
+++ src/index.js
@@ -0,0 +1,1 @@
+export { default as requiredVal } from './requiredVal';
---
+++ src/requiredVal.js
@@ -0,0 +1,25 @@
+export default const requiredVal = (messageId, value, checkboxClass = 'checkbox') => {
+	let messageElement = document.getElementById(messageId);
+	let submitButton = document.getElementById('submit-btn');
+	let errElements = document.getElementsByClass('error');
+	if (value.length <= 0 || value == false && multichecks(checkboxClass) != true) {
+		submitButton.disabled = true;
+		messageElement.style.color = 'red';
+		messageElement.innerHTML = 'This field is reuired';
+	}else{
+		messageElement.style.color = 'green';
+		messageElement.innerHTML = `<i class="success"></i>`;
+	}
+	let errorLength = errElements.length;
+	for (e in errorLength){
+		if(errElements[e].innerHTML == `<i class="success"></i>`){
+			errorLength--;
+		}else{
+			break;
+		}
+		if(errorLength == 0){
+			submitButton.disabled = false;
+		}
+	}
+
+}
